fa5aee496baa24d0296900c2f185891711d017a9
Miki0035 <mknewkebede@gmail.com>
2026-08-13 20:01:59 +0300

    finished usecase, added bloc package

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,38 @@
+import 'package:blog_app/core/error/exception.dart';
+import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_datasource.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDatasource remoteDatasource;
+
+  AuthRepositoryImpl(this.remoteDatasource);
+
+  @override
+  Future<Either<Failure, String>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userId = await remoteDatasource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+}
